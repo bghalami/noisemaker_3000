e5ef3d5161fd6bc4de74e45a0794ce6b1aaea7c6
bghalami <benjaminghalami@gmail.com>
2023-12-04 21:00:12 -0700

    Create noisemaker_3000 class to orchastrate logic

--- lib/noisemaker_3000.rb
+++ lib/noisemaker_3000.rb
@@ -1,3 +1,35 @@
-class Noisemaker3000
+require_relative 'option_handler.rb'
+require_relative 'file_handler.rb'
+require_relative 'noisemaker_logger.rb'
+require_relative 'network_handler.rb'
 
+class Noisemaker3000
+    attr_reader :logger, :options, :command_line
+
+    def initialize(command_line:)
+        @command_line = command_line
+        @logger = NoisemakerLogger.new(command_line: command_line)
+        @options = OptionHandler.new.options
+    end
+
+    def make_some_noise
+        logger.log_process_start
+
+        if FileHandler::VALID_ACTIONS.include?(options[:action])
+            return unless filepath_present?
+
+            FileHandler.new(options, logger: logger).call
+        elsif options[:action] == :network
+            NetworkHandler.new(logger: logger).call
+        end
+    end
+
+    def filepath_present?
+        if options[:filepath].nil? || options[:filepath].empty?
+            puts "Must include valid file path to create, modify, or delete file data"
+            return false
+        end
+
+        true
+    end
 end
